d10829f597a3fccc60fb61ab14c2e8ae7a7406e1
apoowa <98971@student.vitkovickastredni.cz>
2025-03-12 12:07:14 +0100

    m

--- .github/main.py
+++ .github/main.py
@@ -10,10 +10,39 @@
 'program'[2:5]
 #6. Spojte string "Ahoj" a "Slunce"
 "ahoj" + "slunce"
-#7. Napiš program, který se zeptá na jméno, pak na příjmení a pak vypíše iniciály – první písmena zadaných jmeno
-jmeno = input("zadejjmeno: ")
-prijmeni = input("zadej prijmeni: ")
+#7. Napiš program, který se zeptá na jméno, pak na příjmení a pak vypíše iniciály – první písmena zadaných jmen.
+jmeno = input("zadejte jmeno: ")
+prijmeni = input("zadejte prijmeni: ")
 
 inicialy = jmeno[0].upper() + prijmeni[0].upper()
 print("inicialy jsou:", inicialy)
+
 #Výsledný commit - String_done
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#1. Vytvořte pole stringů, které bude obsahovat pět vašich oblíbených měst.
+pole = ["prerov, ostrava, frydek, lednice, kozlovice"]
+
+#2. Vypište toto pole
+print (pole)
+
+#3. Vypište čtvrté město z tohoto pole.
+
+#4. Pomocí cyklu for vypište všechna města pole.
+
+#5. Vypište na obrazovku všechna města v seznamu, a před každé město vypište jeho pořadové číslo.
+
+#6. Přidejte do pole další město.
+
+#7. Odstraňte z pole druhé město.
+
+#8. Vypište upravené pole na obrazovku.
+
+#9. Seřaďte pole měst abecedně.
+
+#10. Napište funkci, která zkontroluje, zda se v poli měst nachází zadané město.
+
+#11. Vytvořte nové pole, který bude obsahovat pouze řetězce, které začínají na písmeno „a“.
+
+#12. Vypište na obrazovku všechna slova, která mají více než 5 písmen.
+
+#Výsledný commit - String_Pole
